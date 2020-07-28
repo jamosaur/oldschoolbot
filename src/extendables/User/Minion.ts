4f72b6485dfd95287f01c30f5830661e1ea7dfce
James Wallen-Jones <j.wallen.jones@googlemail.com>
2020-07-27 20:58:39 -0400

    add minion alching activity

--- src/extendables/User/Minion.ts
+++ src/extendables/User/Minion.ts
@@ -270,6 +270,12 @@ export default class extends Extendable {
 			case Activity.Wintertodt: {
 				return `${this.minionName} is currently fighting the Wintertodt. ${formattedDuration}`;
 			}
+
+			case Activity.Alching: {
+				const data = currentTask as AlchingActivityTaskOptions;
+
+				return `${this.minionName} is currently alching ${data.quantity}x ${data.itemName}. ${formattedDuration}`;
+			}
 		}
 	}
 
